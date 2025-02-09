2438c2083869e78a73f16143367f2dbdfb9a1f75
Saikowo7 <mohammad.arjan@icloud.com>
2025-02-09 17:05:23 +0300

    Updated1

--- app/src/main/java/com/example/salamtech_gp/MainActivity.kt
+++ app/src/main/java/com/example/salamtech_gp/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.salamtech_gp
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+
+        android.os.Handler().postDelayed({
+            val intent = Intent(this, SetupPage::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
         }
+
     }
-}
---
+++ app/src/main/java/com/example/salamtech_gp/SetupPage.kt
@@ -0,0 +1,16 @@
+package com.example.salamtech_gp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SetupPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_setup_page)
+
+    }
+}
